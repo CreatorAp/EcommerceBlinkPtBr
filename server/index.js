60b323821efc5f4852eaf2647debb834d70b4995
Arnon pedro <arnonpdr@gmail.com>
2024-12-24 10:36:50 -0300

    first commi11t2111

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cors({
     credentials : true,
     // origin : process.env.FRONTEND_URL
     // origin : "http://localhost:5173"
-    origin : "https://ecommercefront-etfl9blvv-arnon-pedros-projects.vercel.app/"
+    origin : "https://ecommercefront-etfl9blvv-arnon-pedros-projects.vercel.app"
     
     // origin : "*"
 }))
