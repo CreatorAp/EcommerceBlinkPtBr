c96710c1a49e6778ba7722ee344956010bbb624e
Arnon pedro <arnonpdr@gmail.com>
2025-01-02 10:50:55 -0300

    second1

--- server/index.js
+++ server/index.js
@@ -22,7 +22,10 @@ app.use(cors({
     // origin : "http://localhost:5173"
     // origin : "https://ecommercefront-etfl9blvv-arnon-pedros-projects.vercel.app"
               
-    origin : ["*","http://localhost:5173", "https://ecommercefront-liard.vercel.app", "https://ecommercefront-9q3woacnx-arnon-pedros-projects.vercel.app", "https://ecommercefront-7geguprvq-arnon-pedros-projects.vercel.app", "https://ecommerce-blink-pt-br.vercel.app"]
+    origin : ["*","http://localhost:5173", "https://ecommercefront-liard.vercel.app", 
+        "https://ecommercefront-9q3woacnx-arnon-pedros-projects.vercel.app", 
+        "https://ecommercefront-7geguprvq-arnon-pedros-projects.vercel.app", "https://ecommerce-blink-pt-br.vercel.app", "https://ecommerce-blink-pt-br-front.vercel.app"]
+                                                                              
 }))
 app.use(express.json())
 app.use(cookieParser())
